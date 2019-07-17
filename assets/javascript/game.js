c06917bc22e7f6d67778b042d6de55cebd0d70df
Gabriel <pedromurillo96@gmail.com>
2019-07-17 14:29:03 -0700

    small updates to all files

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+
+var wins = 0;
+var losses = 0; 
+var guessLeft = 9;
+
+
+
+document.onkeyup = function (event){
+
+}
