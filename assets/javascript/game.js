555db2bab74ae0cc36abe40b90dd0a4847baebce
Gabriel <pedromurillo96@gmail.com>
2019-07-18 15:13:58 -0700

    final updates to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 var wins = 0;
 var losses = 0; 
 var guessLeft = 9;
+var guessSoFar = []
 
 var winsText = document.getElementById("win");
 var lossesText = document.getElementById("loss");
@@ -12,28 +13,42 @@ var userChoiceText = document.getElementById("far");
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
+
+
 document.onkeyup = function (event){
     var userGuess = event.key;
+    
 
 
     if (userGuess === computerGuess){
         wins++;
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         guessLeft = 9;
+        //guessSoFar.pop(userGuess);
+        guessSoFar = [];
     }
     
-    if (userGuess !== computerGuess){
-        guessLeft--;
-    }
 
     if (guessLeft === 0){
         losses++;
         guessLeft = 9;
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        //guessSoFar.pop(userGuess);
+        guessSoFar = [];
     }
+
+    if (userGuess !== computerGuess){
+        guessLeft--;
+        guessSoFar.push(userGuess);
+        
+        
+        
+    }
+
+    
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     leftText.textContent = "Guesses Left: " + guessLeft;
-    userChoiceText.textContent = "Your Guesses so Far: " + userGuess;
+    userChoiceText.textContent = "Your Guesses so Far: " + guessSoFar;
     
 }
