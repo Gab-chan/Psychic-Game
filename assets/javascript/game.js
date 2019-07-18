07d3de4f20237a541fff2eaeaf768cd982dcc8eb
Gabriel <pedromurillo96@gmail.com>
2019-07-17 20:18:42 -0700

    major updates to js file and minor updates to html file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,36 @@ var wins = 0;
 var losses = 0; 
 var guessLeft = 9;
 
+var winsText = document.getElementById("win");
+var lossesText = document.getElementById("loss");
+var leftText = document.getElementById("left");
+var userChoiceText = document.getElementById("far");
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
 document.onkeyup = function (event){
+    var userGuess = event.key;
 
+
+    if (userGuess === computerGuess){
+        wins++;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        guessLeft = 9;
+    }
+    
+    if (userGuess !== computerGuess){
+        guessLeft--;
+    }
+
+    if (guessLeft === 0){
+        losses++;
+        guessLeft = 9;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    leftText.textContent = "Guesses Left: " + guessLeft;
+    userChoiceText.textContent = "Your Guesses so Far: " + userGuess;
+    
 }
